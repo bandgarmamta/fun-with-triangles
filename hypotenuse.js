dcc5ce658d9af1dacb03613a2dbc03858bd783b2
bandgarmamta <bandgarmamta@gmail.com>
2021-09-29 08:07:14 +0530

    update html js

---
+++ hypotenuse.js
@@ -0,0 +1,17 @@
+const sides = document.querySelectorAll(".side-input");
+const hypBtn = document.querySelector("btn-submit");
+const outputEl = document.querySelector("#output");
+
+function calculateSumOfSquares(a,b){
+    const sumOfSquares = a*a + b*b;
+    return sumOfSquares;
+}
+
+function calculateHypotenuse(){
+    const sumOfSquares = calculateSumOfSquares(Number(sides[0].value), sides[1].value);
+    const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
+    outputEl.innerText = "The length of hypotenuse is" + lengthOfHypotenuse;
+}
+
+hypBtn.addEventListener("click", calculateHypotenuse);
+
--- istriangle.js
+++ istriangle.js
@@ -9,7 +9,11 @@ function claculateSunOfAngles(angle1, angle2, angle3){
 
 function isTriangle(){
     const sumOfAngles = claculateSunOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value) )
-
+    if(sumOfAngles === 180){
+        outputEl.innerText = "yay!, the angles for a triangle";
+    }else{
+        outputEl.innerText="Oh! Oh!, the angles don't form a triangle";
+    }
 }
 
 isTriangleBtn.addEventListener("click",isTriangle);
